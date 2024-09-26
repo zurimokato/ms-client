a00c886d875e7d8ab3a2ac1d0ff992351c2d64a4
zurimokato <noj2304@gmail.com>
2024-09-25 22:48:39 -0500

    client microserice

---
+++ src/main/java/com/local/ms_client/MsClientApplication.java
@@ -0,0 +1,13 @@
+package com.local.ms_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientApplication.class, args);
+	}
+
+}
